615837d21c83f5731e4e89c8d7b25ffd27bf6441
unknown <fabellaivan@hotmail.com>
2022-05-21 12:12:04 -0300

    Subida Parcial

---
+++ Parcial 1/src/Calculos.h
@@ -0,0 +1,14 @@
+/*
+ * Calculos.h
+ *
+ *  Created on: 18 may 2022
+ *      Author: Usuario Programador
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+
+float dataCollection(datosCenso* datos, int lenDatosCenso);
+float promediar(int valorPivot, int valorPromediar);
+
+#endif /* CALCULOS_H_ */
